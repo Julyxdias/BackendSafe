d52b5c90f70318a18066a5faa172889b325c3f91
Karolaine Helena Santos Silva <153235224+Karolaine231@users.noreply.github.com>
2026-04-07 09:54:15 -0300

    Modify CORS middleware settings
    Updated CORS settings to allow additional origins and credentials.
    
    Signed-off-by: Karolaine Helena Santos Silva <153235224+Karolaine231@users.noreply.github.com>

--- main.py
+++ main.py
@@ -36,7 +36,13 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://energy-safe-9m2q.vercel.app"],
+    allow_origins=[
+        "https://energy-safe.vercel.app",
+        "https://energy-safe-9m2q.vercel.app",
+        "http://localhost:5500",
+        "http://127.0.0.1:5500",
+    ],
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
